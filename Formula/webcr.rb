b6c096af8bb1f9ae23d1afcdaf4b263998ed1cf6
AndeyM <andrew@webcrawlerapi.com>
2026-04-22 16:36:28 +0200

    brew(webcr): update to v0.0.3

--- Formula/webcr.rb
+++ Formula/webcr.rb
@@ -1,14 +1,15 @@
 class Webcr < Formula
   desc "Turn websites into LLM-ready markdown from the command line with WebCrawlerAPI"
   homepage "https://webcrawlerapi.com"
-  url "https://github.com/webCrawlerAPI/webcr/archive/refs/tags/v0.0.2.tar.gz"
-  sha256 "a710d75198efe65a2b083702cb4cdefdccc191f0c67196e23397c1f721e4ab8b"
+  url "https://github.com/webCrawlerAPI/webcr/archive/refs/tags/v0.0.3.tar.gz"
+  sha256 "e90f1d3d59611ef34c19137ad420d8bd743a72ead01bec94ef02947fc9965818"
   license "MIT"
 
   depends_on "node"
 
   def install
     libexec.install "webcr.js", "src", "README.md", "LICENSE", "package.json"
+    (libexec/"webcr.js").chmod 0555
 
     (bin/"webcr").write_env_script libexec/"webcr.js", PATH: ENV["PATH"]
   end
